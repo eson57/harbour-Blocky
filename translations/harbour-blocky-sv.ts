15efc56cf4510dab3fc0b37042f48c6ced01d017
Åke Engelbrektson <eson57@users.noreply.github.com>
2026-06-09 15:30:28 +0200

    Create harbour-blocky-sv.ts
    Add Swedish translation, as weblate link doesn't exist.

---
+++ translations/harbour-blocky-sv.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>%1m %2s</source>
+        <translation>%1m %2s</translation>
+    </message>
+    <message>
+        <source>%1m</source>
+        <translation>%1m</translation>
+    </message>
+    <message>
+        <source>%1s</source>
+        <translation>%1s</translation>
+    </message>
+    <message>
+        <source>Blocking disabled for:
+%1</source>
+        <translation>Blockering avaktiverad för:
+%1</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>DNS server address</source>
+        <translation>DNS serveradress</translation>
+    </message>
+    <message>
+        <source>Save and restart</source>
+        <translation>Spara och starta om</translation>
+    </message>
+    <message>
+        <source>ERROR! blocky start failed with: %1</source>
+        <translation>FEL! Blockys start misslyckades med: %1</translation>
+    </message>
+    <message>
+        <source>DNS</source>
+        <translation>DNS</translation>
+    </message>
+    <message>
+        <source>Add DNS</source>
+        <translation>Lägg till DNS</translation>
+    </message>
+    <message>
+        <source>Denylists</source>
+        <translation>Blocklistor</translation>
+    </message>
+    <message>
+        <source>Raw configuration</source>
+        <translation>Raw-konfiguration</translation>
+    </message>
+    <message>
+        <source>The generated raw configuration:</source>
+        <translation>Genererad raw-konfiguration:</translation>
+    </message>
+    <message>
+        <source>Add sources to denylists to block ads and trackers. See &lt;a href=&quot;https://0xerr0r.github.io/blocky/latest/configuration/#definition-allowdenylists&quot;&gt;Blocky documentation&lt;/a&gt; for more details.</source>
+        <translation>Lägg till källor i blocklistorna för att blockera annonser och spårare. Se &lt;a href=&quot;https://0xerr0r.github.io/blocky/latest/configuration/#definition-allowdenylists&quot;&gt;Blocky-dokumentationen&lt;/a&gt; för mer information.</translation>
+    </message>
+    <message>
+        <source>URL, domain, wildcard or regex</source>
+        <translation>URL, domän, jokertecken eller regex</translation>
+    </message>
+    <message>
+        <source>Add to denylist</source>
+        <translation>Lägg till i blocklistan</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Default duration for temporarily disabling blocking from the Cover.</source>
+        <translation>Standardtid för att tillfälligt inaktivera blockering från appminiatyren.</translation>
+    </message>
+    <message>
+        <source>Rescue</source>
+        <translation>Räddning</translation>
+    </message>
+    <message>
+        <source>Restore default configuration</source>
+        <translation>Återställ standardkonfiguration</translation>
+    </message>
+    <message>
+        <source>Restoring default configuration</source>
+        <translation>Återställer standardkonfiguration</translation>
+    </message>
+    <message>
+        <source>REST API</source>
+        <translation>REST API</translation>
+    </message>
+    <message>
+        <source>Enable</source>
+        <translation>Aktivera</translation>
+    </message>
+    <message>
+        <source>Enables the REST API on port 4000. Required to disable blocking from the Cover. Also, the service must be restarted for this change to take effect.</source>
+        <translation>Aktiverar REST API:n på port 4000. Krävs för att inaktivera blockering från miniatyren. Dessutom måste tjänsten startas om för att denna ändring ska träda i kraft.</translation>
+    </message>
+    <message>
+        <source>Blocking settings</source>
+        <translation>Blockeringsinställningar</translation>
+    </message>
+    <message>
+        <source>Default duration (seconds)</source>
+        <translation>Standard varaktighet (sekunder)</translation>
+    </message>
+    <message>
+        <source>Restores the Blocky configuration to the default shipped with the app and restarts the service.</source>
+        <translation>Återställer Blocky-konfigurationen till standardinställningarna som levereras med appen och startar om tjänsten.</translation>
+    </message>
+</context>
+</TS>
